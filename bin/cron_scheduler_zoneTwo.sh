3bffd6f70a91d5ef05a2893c011584af2f01229d
oreilp25 <mattphew_stink@hotmail.com>
2014-04-02 21:34:11 +0100

    fixing errors in file paths and query names script two and three

--- bin/cron_scheduler_zoneTwo.sh
+++ bin/cron_scheduler_zoneTwo.sh
@@ -2,10 +2,10 @@
 
 #script to be put on cron will query schedules table to determine if gpio value should
 # be 1 or 0.
-GPIO_VALUE="/home/peter/Desktop/GPIO_Simulator/gpio3/value"
+GPIO_VALUE="/home/peter/Desktop/GPIO_Simulator/gpio2/value"
 CRON_LOG="/home/peter/Desktop/GPIO_Simulator/zoneSchedulers.log"
 
-QUERY="mysql --skip-column-names -uroot -pshroot ThermoPi -e 'select POWERED_ON from SCHEDULE_ZONE_THREE where (TIME_RANGE = getTime(now()))and (ZONE_ID = 1);'"
+QUERY="mysql --skip-column-names -uroot -pshroot ThermoPi -e 'select POWERED_ON from SCHEDULE_ZONE_TWO where (TIME_RANGE = getTime(now()))and (ZONE_ID = 1);'"
 
 EXECUTE=$(eval $QUERY);
 
@@ -13,7 +13,7 @@ function compareToValueFile {
 	VAL=`cat $GPIO_VALUE`;
 	
 	if [ $VAL -eq $1 ] ; then 
-		echo -e `date` " [ZONE THREE]: no state change required in value: Current value -> $VAL" >> $CRON_LOG;
+		echo -e `date` " [ZONE TWO]: no state change required in value: Current value -> $VAL" >> $CRON_LOG;
 		exit 0;
 	fi
 }
@@ -21,12 +21,12 @@ function compareToValueFile {
 if [ $EXECUTE -gt 0 ] ; then
 	compareToValueFile 1;
 	echo "1" > $GPIO_VALUE;
-	echo -e `date` " [ZONE THREE]: GPIO1 turned ON: " >> $CRON_LOG;
+	echo -e `date` " [ZONE TWO]: GPIO2 turned ON: " >> $CRON_LOG;
 
 else 
 	compareToValueFile 0;
 	echo "0" > $GPIO_VALUE;
-	echo -e `date`  " [ZONE THREE]: GPIO1 turned OFF." >> $CRON_LOG;
+	echo -e `date`  " [ZONE TWO]: GPIO2 turned OFF." >> $CRON_LOG;
 fi
 
 exit 0;
