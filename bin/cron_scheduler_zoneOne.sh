20e051612ee125f1252dd0f1e5db8016acdb8b44
oreilp25 <mattphew_stink@hotmail.com>
2014-04-02 21:08:02 +0100

    adding scripts for zones 1-3 which will query respective zone tables and either write 1 or 0 to gpio - FilePaths need to be modifiedgit stage .

---
+++ bin/cron_scheduler_zoneOne.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#script to be put on cron will query schedules table to determine if gpio value should
+# be 1 or 0.
+GPIO_VALUE="/home/peter/Desktop/GPIO_Simulator/gpio1/value"
+CRON_LOG="/home/peter/Desktop/GPIO_Simulator/zoneSchedulers.log"
+
+QUERY="mysql --skip-column-names -uroot -pshroot ThermoPi -e 'select POWERED_ON from SCHEDULE_ZONE_ONE where (TIME_RANGE = getTime(now()))and (ZONE_ID = 1);'"
+
+EXECUTE=$(eval $QUERY);
+
+function compareToValueFile {
+	VAL=`cat $GPIO_VALUE`;
+	
+	if [ $VAL -eq $1 ] ; then 
+		echo -e `date` "  [ZONE ONE]: no state change required in value: Current value -> $VAL" >> $CRON_LOG;
+		exit 0;
+	fi
+}
+
+if [ $EXECUTE -gt 0 ] ; then
+	compareToValueFile 1;
+	echo "1" > $GPIO_VALUE;
+	echo -e `date` "  [ZONE ONE]: GPIO1 turned ON: " >> $CRON_LOG;
+
+else 
+	compareToValueFile 0;
+	echo "0" > $GPIO_VALUE;
+	echo -e `date` "  [ZONE ONE]: GPIO1 turned OFF."  >> $CRON_LOG;
+fi
+
+exit 0;
